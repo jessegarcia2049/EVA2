fbac43948bc9487ca10b6aa3aa34b48af23b5f5d
jessegarcia2049 <159494595+jessegarcia2049@users.noreply.github.com>
2024-03-06 16:52:59 -0600

    EVA2_3_IF
    OPERADORES LOGICOS
    ==, &&, ||, !
    
    EVA_2_OPERADORES_LOGICOS
    
    LOGICA BOOLEANA
    AND OR NOT  
    
    AND	 Y  
    
    OR	O 
    
    NOT     NO
    
    
    trabajamos con true or false 
    
    boolean valBol1, valBol2;
    valBol1 = false;
    valBol2 = false;
    boolean resu; 
    //OPERADOR AND --arrow &&
    resu valBol1 && valBol2;
    System.out.println ("Resu = " + resu)
    
    OPERADORES ARIMETICOS:
    
    +-* / += -= /=

---
+++  EVA_2_OPERADORES_LOGICOS/JavaApplication70/src/javaapplication70/JavaApplication70.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication70;
+
+/**
+ *
+ * @author invitado
+ */
+public class JavaApplication70 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+   boolean A,B;
+   A= false;
+   B= false;
+   boolean resu;
+ //OPERADOR AND --arrow &&
+  resu= A && B;
+System.out.println("Resu = " + resu);
+          A= false;
+          B= false;
+           resu= A && B;
+System.out.println("Resu = " + resu);
+          A= false;
+          B= false;
+           resu= A && B;
+System.out.println("Resu = " + resu);
+          A= false;
+          B= false;
+    resu= A || B; 
+         System.out.println("Resu (or) = " + resu);
+          A= false;
+          B= false;
+    resu= A || B; 
+         System.out.println("Resu (or) = " + resu);
+// NEGACION -->> !
+A = false;
+resu =!A;
+        System.out.println("Resu (!)=" + resu);
+    }
+    }
