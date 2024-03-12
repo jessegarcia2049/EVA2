b4baab135cdf28180c2bc543f96e29aa80f62ebe
jessegarcia2049 <159494595+jessegarcia2049@users.noreply.github.com>
2024-03-12 16:49:43 -0600

    Add files via upload

---
+++ EVA2_8_VOLADO/EVA2_8_VOLADO/src/eva2_8_volado/EVA2_8_VOLADO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_8_volado;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_8_VOLADO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  // numero aleatorio en Java:
+  // valores entre 0 y 1 
+  // 0.3135343553354
+    //
+  //
+  
+  double moneda;
+          moneda = Math.random();
+          System.out.println("El valor de la moneda =" + moneda);
+    
+          //0. es la mitad del rango 
+          if ( moneda > 0.5)
+               System.out.println("MONEDA");
+          else 
+              System.out.println("CRUZ");
+    //// SEGUN LO QUE SALGA EL USUARIO GANA O PIERDE 
+    
+  // tarea consecutiva usar mismos valores y variables. 
+  // ES DECIR BASADO MUCHO EN ESTE EL SIGUIENTE. 
+  
+    
+    }
+    
+}
